23c23dcd0bceeaafa47f04914f18908a6374ae29
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-30 00:06:12 +0200

    first commit

---
+++ src/main/java/com/hugocg21/Gestion_Banco_Backend/GestionBancoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hugocg21.Gestion_Banco_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBancoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBancoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugocg21/Gestion_Banco_Backend/GestionBancoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugocg21.Gestion_Banco_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBancoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
